33b9145954e432555afda5e25bc6b67e8ff646aa
Mateguo1 <gzypro@hotmail.com>
2024-11-28 12:28:20 +0800

    update

--- trainers/ConcepPath.py
+++ trainers/ConcepPath.py
@@ -1,3 +1,5 @@
+import os.path as osp
+
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -16,12 +18,13 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # device = torch.device("cpu")
 
 def orthogonal_loss(features, gamma=0.5):
-    
+    # print(features.shape)
     if len(features.shape)<3:
+        # print("aaaaa")
         features = features.unsqueeze(0)
-
+    # print(features.shape)
     device = (torch.device('cuda') if features.is_cuda else torch.device('cpu'))
-    # features are normalized
+    #  features are normalized
     features = F.normalize(features, p=2, dim=1)
     labels = torch.arange(features.shape[-1])
     labels = labels[:, None]  # extend dim
@@ -79,11 +82,11 @@ class Adapter(nn.Module):
 
 class PromptLearner(nn.Module):
     # def __init__(self, cfg, classnames, clip_model):
-    def __init__(self, classnames, clip_model, n_ctx, n_flp=0, num_patch_prompt=0, is_shared=False):
+    def __init__(self, classnames, clip_model, n_ctx, n_ddp=0, num_patch_prompt=0, is_shared=False):
         super().__init__()
         n_cls = len(classnames)
         
-        self.n_flp = n_flp
+        self.n_ddp = n_ddp
         self.num_patch_prompt = num_patch_prompt
 
         # ===============
@@ -113,16 +116,16 @@ class PromptLearner(nn.Module):
 
         else:
         # random initialization
-            if not is_shared:
+            if not is_shared: # 是否class-wise
                 print("Initializing class-specific contexts")
                 ctx_vectors = torch.empty(n_cls, n_ctx, ctx_dim, dtype=dtype)
-                if n_flp>0 and num_patch_prompt>0:
-                    flp_vectors = torch.empty(int(n_cls/num_patch_prompt)*n_flp, 75, ctx_dim, dtype=dtype)
+                if n_ddp>0 and num_patch_prompt>0:
+                    ddp_vectors = torch.empty(int(n_cls/num_patch_prompt)*n_ddp, 75, ctx_dim, dtype=dtype)
                 
             else:
                 print("Initializing a generic context")
-                if n_flp>0 and num_patch_prompt>0:
-                    flp_vectors = torch.empty(75, ctx_dim, dtype=dtype)
+                if n_ddp>0 and num_patch_prompt>0:
+                    ddp_vectors = torch.empty(75, ctx_dim, dtype=dtype)
                     
                 ctx_vectors = torch.empty(n_ctx, ctx_dim, dtype=dtype)
                 
@@ -138,7 +141,8 @@ class PromptLearner(nn.Module):
         name_lens = [len(_tokenizer.encode(name)) for name in classnames]
         prompts = [prompt_prefix + " " + name + "." for name in classnames]
         
- 
+        # else:
+        # print(prompts)
         tokenized_prompts = torch.cat([clip.tokenize(p) for p in prompts]).to(device)
         
         with torch.no_grad():
@@ -151,25 +155,25 @@ class PromptLearner(nn.Module):
         self.register_buffer("token_suffix", embedding[:, 1 + n_ctx :, :])  # CLS, EOS
 
         
-        # initiall the data-driven parameters
-        if n_flp>0 and num_patch_prompt>0:
-            nn.init.normal_(flp_vectors, std=0.02)
-            self.flp = nn.Parameter(flp_vectors)
-            flp_prefix = " ".join(["X"] * 75)
-            tokenized_flp = torch.cat([clip.tokenize(flp_prefix+".") for _ in range(int(n_cls/num_patch_prompt)*n_flp)]).to(device)
+        # 初始化全学习参数
+        if n_ddp>0 and num_patch_prompt>0:
+            nn.init.normal_(ddp_vectors, std=0.02)
+            self.ddp = nn.Parameter(ddp_vectors)
+            ddp_prefix = " ".join(["X"] * 75)
+            tokenized_ddp = torch.cat([clip.tokenize(ddp_prefix+".") for _ in range(int(n_cls/num_patch_prompt)*n_ddp)]).to(device)
             
             with torch.no_grad():
-                embedding_flp = clip_model.token_embedding(tokenized_flp).type(dtype)
+                embedding_ddp = clip_model.token_embedding(tokenized_ddp).type(dtype)
             
-            self.register_buffer("flp_token_prefix", embedding_flp[:, :1, :])  # SOS
-            self.register_buffer("flp_token_suffix", embedding_flp[:, 1 + 75 :, :])  # CLS, EOS
+            self.register_buffer("ddp_token_prefix", embedding_ddp[:, :1, :])  # SOS
+            self.register_buffer("ddp_token_suffix", embedding_ddp[:, 1 + 75 :, :])  # CLS, EOS
             
             tokenized_prompts_ = []
             for i in range(n_cls):
                 if i%num_patch_prompt==0:
                     cur_i_ = int(i/num_patch_prompt)
-                    # print(tokenized_flp.shape)
-                    tokenized_prompts_.append(tokenized_flp[cur_i_:cur_i_+n_flp])
+                    # print(tokenized_ddp.shape)
+                    tokenized_prompts_.append(tokenized_ddp[cur_i_:cur_i_+n_ddp])
                 tokenized_prompts_.append(tokenized_prompts[i].unsqueeze(0))
             self.tokenized_prompts = torch.cat(tokenized_prompts_, dim=0).to(device)
             # print("aaaa")
@@ -182,18 +186,18 @@ class PromptLearner(nn.Module):
         self.name_lens = name_lens
         
         # ===============
-        self.class_token_position = "pre"
+        self.class_token_position = "top" #cfg.TRAINER.COOP.CLASS_TOKEN_POSITION
         # ===============
 
     def forward(self):
         ctx = self.ctx
         
-        if self.n_flp>0 and self.num_patch_prompt>0:
-            flp_prefix = self.flp_token_prefix
-            flp_suffix = self.flp_token_suffix
-            flp = self.flp
-            if flp.dim() == 2:
-                flp = flp.unsqueeze(0).expand(int(self.n_cls/self.num_patch_prompt)*self.n_flp, -1, -1)
+        if self.n_ddp>0 and self.num_patch_prompt>0:
+            ddp_prefix = self.ddp_token_prefix
+            ddp_suffix = self.ddp_token_suffix
+            ddp = self.ddp
+            if ddp.dim() == 2:
+                ddp = ddp.unsqueeze(0).expand(int(self.n_cls/self.num_patch_prompt)*self.n_ddp, -1, -1)
             
         if ctx.dim() == 2:
             ctx = ctx.unsqueeze(0).expand(self.n_cls, -1, -1)
@@ -202,29 +206,29 @@ class PromptLearner(nn.Module):
         suffix = self.token_suffix
         
         # ==== 
-        if self.class_token_position == "pre":
+        if self.class_token_position == "top":
             prompts = []
             for i in range(self.n_cls):
-                if self.n_flp>0 and self.num_patch_prompt>0:
+                if self.n_ddp>0 and self.num_patch_prompt>0:
                     if i%self.num_patch_prompt==0:
 
                         cur_i_ = int(i/self.num_patch_prompt)
                     
-                        flp_i = flp[cur_i_: cur_i_+self.n_flp, :, :]
-                        flp_prefix_i = flp_prefix[cur_i_: cur_i_+self.n_flp, :, :]
-                        flp_suffix_i = flp_suffix[cur_i_: cur_i_+self.n_flp, :, :]
+                        ddp_i = ddp[cur_i_: cur_i_+self.n_ddp, :, :]
+                        ddp_prefix_i = ddp_prefix[cur_i_: cur_i_+self.n_ddp, :, :]
+                        ddp_suffix_i = ddp_suffix[cur_i_: cur_i_+self.n_ddp, :, :]
                         
-                        prompt_flp = torch.cat(
+                        prompt_ddp = torch.cat(
                                 [
-                                    flp_prefix_i,
-                                    flp_i,
-                                    flp_suffix_i
+                                    ddp_prefix_i,
+                                    ddp_i,
+                                    ddp_suffix_i
                                 ],
                                 dim=1,
                                 ) 
                         
                         prompts.append(
-                            prompt_flp
+                            prompt_ddp
                         )
                 # tokenized_prompts.append(clip.tokenize(prompts[cur_i]))
                 name_len = self.name_lens[i]
@@ -237,7 +241,7 @@ class PromptLearner(nn.Module):
                         prefix_i,  # (1, 1, dim)
                         class_i,   # (1, name_len, dim)
                         ctx_i,     # (1, n_ctx, dim)
-                        suffix_i,  # (1, *, dim) 
+                        suffix_i,  # (1, *, dim) # 根据上面的内容变化
                     ],
                     dim=1,
                 )
@@ -247,25 +251,40 @@ class PromptLearner(nn.Module):
         return prompts
 
 
-class ConceptPath(nn.Module):
-    def __init__(self, slide_prompt, patch_prompt, clip_model, loss_func, num_patch_prompt=26, n_ctx=16, n_ddp=0, is_shared=False, orth_ratio=2, adapt_ratio=0):
+class ConcepPath(nn.Module):
+    def __init__(self, 
+                 slide_prompt, 
+                 patch_prompt, 
+                 clip_model, 
+                 loss_func, 
+                 n_classes, 
+                #  attn_type, 
+                 num_patch_prompt=26, 
+                 mask_ratio=0, 
+                 n_ctx=16, 
+                 n_ddp=0, 
+                 is_shared=False, 
+                 orth_ratio=0.2, 
+                 weighted_type="p2c", 
+                 is_adapted=False, 
+                 tr_ratio=0):
+        
         super().__init__()
         
-        n_flp = n_ddp
-        
-        self.num_patch_prompt_ = num_patch_prompt+n_flp
-        
+        self.num_patch_prompt_ = num_patch_prompt+n_ddp
+        self.weighted_type = weighted_type
+            
         self.orth_ratio = orth_ratio
         
         self.clip_model = clip_model
         
-        self.n_flp = n_ddp
-        
-        self.patch_prompt_learner = PromptLearner(patch_prompt, clip_model, n_ctx, n_flp, num_patch_prompt, is_shared=is_shared)
+        self.n_ddp = n_ddp
+        self.patch_prompt_learner = PromptLearner(patch_prompt, clip_model, n_ctx, n_ddp, num_patch_prompt, is_shared=is_shared)
         self.slide_prompt_learner = PromptLearner(slide_prompt, clip_model, n_ctx, is_shared=is_shared)
-        
+       
         self.patch_tokenized_prompts = self.patch_prompt_learner.tokenized_prompts
         self.slide_tokenized_prompts = self.slide_prompt_learner.tokenized_prompts
+
         self.text_encoder = TextEncoder(clip_model)
         
         self.loss_func = loss_func
@@ -273,15 +292,20 @@ class ConceptPath(nn.Module):
         self.logit_scale = clip_model.logit_scale
         self.dtype = clip_model.ln_final.weight.dtype
         
-        self.adapt_ratio = adapt_ratio
+        self.is_adapted = is_adapted
         
-        if self.adapt_ratio>0:
-            self.text_adapter = Adapter(512)
-            self.feature_adapter = Adapter(512)
+        self.patch_adapt_ratio = nn.Parameter(torch.tensor(0.2))
+        self.slide_adapt_ratio = nn.Parameter(torch.tensor(0.2))
+        
+        if is_adapted:
+            self.slide_text_adapter = Adapter(512)
+            self.slide_feature_adapter = Adapter(512)
+            
 
     def forward(self, patch_features, label, result_fp=None, test=False):
         
         patch_features = patch_features.squeeze(0)
+
         patch_features = patch_features.type(self.dtype)
         patch_prompts = self.patch_prompt_learner()
         patch_tokenized_prompts = self.patch_tokenized_prompts
@@ -289,10 +313,13 @@ class ConceptPath(nn.Module):
         slide_prompts = self.slide_prompt_learner()
         slide_tokenized_prompts = self.slide_tokenized_prompts
         
-
         patch_text_features = self.text_encoder(patch_prompts, patch_tokenized_prompts)
         slide_text_features = self.text_encoder(slide_prompts, slide_tokenized_prompts)
-
+        
+        patch_features = patch_features / patch_features.norm(dim=-1, keepdim=True)
+        
+        slide_adapt_ratio = 0.2
+        
         patch_features = patch_features / patch_features.norm(dim=-1, keepdim=True)
         patch_text_features = patch_text_features / patch_text_features.norm(dim=-1, keepdim=True)
         slide_text_features = slide_text_features / slide_text_features.norm(dim=-1, keepdim=True)
@@ -303,8 +330,7 @@ class ConceptPath(nn.Module):
             feature_fp = result_fp.replace("attn_score", "feature")
             import os
             feature_rp = "/".join(feature_fp.split("/")[:-1])
-            if not os.path.exists(feature_rp):
-                os.makedirs(feature_rp)
+            os.makedirs(feature_rp, exist_ok=True)
             with open(feature_rp+"/feature.pkl", 'wb') as file:
                 pickle.dump(out_put, file)
 
@@ -313,46 +339,53 @@ class ConceptPath(nn.Module):
         slide_text_features = slide_text_features.type(self.dtype)
         
         # oth loss for patch text features
-        orth_loss = orthogonal_loss(patch_text_features.reshape(-1, self.num_patch_prompt_, 512).transpose(1,2))
+        loss_i = orthogonal_loss(patch_text_features.reshape(-1, self.num_patch_prompt_, 512).transpose(1,2))
         
         sim_matrix = F.softmax(logit_scale*patch_features@patch_text_features.t(), dim=1)
         
         if test:
             import pickle
-            out_put = {"name": result_fp.split("/")[-1], "att_score": sim_matrix}
-            with open(result_fp, 'wb') as file:
+            attention_score = sim_matrix
+            out_put = {"name": result_fp.split("/")[-1], "attention_score": attention_score}
+            with open(result_fp.replace(".pkl", "_attention_score.pkl"), 'wb') as file:
                 pickle.dump(out_put, file)
         
         num_patchs, _ = sim_matrix.shape
         
+        ''' 4.2 slide feature part-mean '''
         slide_features = sim_matrix.t() @ patch_features
         _, embedding_len = slide_features.shape
         slide_features = slide_features/slide_features.norm(dim=-1, keepdim=True)
         slide_features = slide_features.reshape(-1, self.num_patch_prompt_, embedding_len)
-    
+
         slide_features_ = torch.bmm(
                 slide_text_features.unsqueeze(1), 
                 patch_text_features.reshape(-1, self.num_patch_prompt_, embedding_len).transpose(1, 2),
             ).squeeze(1)
         slide_features_ = slide_features_/torch.sum(slide_features_, dim=-1, keepdim=True)
-        slide_features_ = slide_features+slide_features_.unsqueeze(-1)*slide_features
-
-        slide_features = slide_features_/slide_features_.norm(dim=-1, keepdim=True)
-
-        slide_features = torch.mean(slide_features, dim=1)
-
-        slide_features = slide_features / slide_features.norm(dim=-1, keepdim=True)
+        slide_features = slide_features+slide_features_.unsqueeze(-1)*slide_features
             
+            
+        if test:
+            import pickle
+            patch_prompt_score = slide_features_.unsqueeze(-1)
+            out_put = {"name": result_fp.split("/")[-1], "patch_prompt_score": patch_prompt_score}
+            with open(result_fp.replace(".pkl", "_patch_prompt_score.pkl"), 'wb') as file:
+                pickle.dump(out_put, file)
+            
+        slide_features = slide_features/slide_features.norm(dim=-1, keepdim=True)
         
-        if self.adapt_ratio>0:
-            adapted_slide_features = self.feature_adapter(slide_features)
-            slide_features = self.ratio * adapted_slide_features/adapted_slide_features.norm(dim=-1, keepdim=True) + (1 - self.ratio) * slide_features
-            
-            adapted_slide_text_features = self.text_adapter(slide_text_features)
-            slide_text_features = self.ratio * adapted_slide_text_features/adapted_slide_text_features.norm(dim=-1, keepdim=True) + (1 - self.ratio) * slide_text_features
-            
+        slide_features = torch.mean(slide_features, dim=1)
         slide_features = slide_features / slide_features.norm(dim=-1, keepdim=True)
-        slide_text_features = slide_text_features / slide_text_features.norm(dim=-1, keepdim=True)
+        
+        if self.is_adapted:
+            adapted_slide_features = self.slide_feature_adapter(slide_features)
+            slide_features = slide_adapt_ratio * adapted_slide_features/adapted_slide_features.norm(dim=-1, keepdim=True) + (1 - slide_adapt_ratio) * slide_features
+            slide_features = slide_features / slide_features.norm(dim=-1, keepdim=True)
+            
+            adapted_slide_text_features = self.slide_feature_adapter(slide_text_features)
+            slide_text_features = slide_adapt_ratio * adapted_slide_text_features/adapted_slide_text_features.norm(dim=-1, keepdim=True) + (1 - slide_adapt_ratio) * slide_text_features
+            slide_text_features = slide_text_features / slide_text_features.norm(dim=-1, keepdim=True)
         
         logits = torch.diag(logit_scale*slide_features @ slide_text_features.t()).unsqueeze(0)
         
@@ -360,6 +393,14 @@ class ConceptPath(nn.Module):
 
         Y_hat = torch.topk(logits, 1, dim=1)[1]
         
+        if test:
+            print(f" \
+                概率为：{Y_prob}\n \
+                分类为：{Y_hat}\n \
+                attention score 为：{attention_score}\n \
+                patch_prompt_score 为：{patch_prompt_score} \
+            ")
+        
         loss = self.loss_func(logits, label)
 
-        return logits, Y_prob, Y_hat, loss + self.orth_ratio*orth_loss.sum()
+        return logits, Y_prob, Y_hat, loss+self.orth_ratio*loss_i.sum()
